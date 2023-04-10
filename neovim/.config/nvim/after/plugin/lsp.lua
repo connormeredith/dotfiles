ab975507d30bcdd525da437d83231701152456bc
Connor Meredith <4907463+connormeredith@users.noreply.github.com>
2023-04-10 22:02:42 +0100

    Added neovim config.

---
+++ neovim/.config/nvim/after/plugin/lsp.lua
@@ -0,0 +1,15 @@
+local lsp = require('lsp-zero').preset({})
+
+lsp.ensure_installed({
+  'tsserver',
+  'rust_analyzer',
+})
+
+lsp.on_attach(function(client, bufnr)
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+lsp.setup()
---
+++ neovim/.config/nvim/lua/custom/set.lua
@@ -0,0 +1,18 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 10
+
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "
