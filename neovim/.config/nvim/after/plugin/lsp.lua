da6fa47979d7c6aea4a9d14f050b7039e610deec
Connor Meredith <4907463+connormeredith@users.noreply.github.com>
2023-04-15 16:07:08 +0100

    Configure format on save

--- neovim/.config/nvim/after/plugin/lsp.lua
+++ neovim/.config/nvim/after/plugin/lsp.lua
@@ -22,10 +22,23 @@ lsp.configure("lua_ls", {
 lsp.setup()
 
 local null_opts = lsp.build_options("null-ls", {})
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
   on_attach = function(client, bufnr)
     null_opts.on_attach(client, bufnr)
+
+    -- Format on save.
+    if client.supports_method("textDocument/formatting") then
+      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end,
+      })
+    end
   end,
   sources = {
     null_ls.builtins.diagnostics.eslint.with({
