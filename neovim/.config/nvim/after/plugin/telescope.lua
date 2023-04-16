8b8f92143b2f50590a667d0b4c0056209e0cabb1
Connor Meredith <4907463+connormeredith@users.noreply.github.com>
2023-04-16 17:50:19 +0100

    Telescope improvements

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -1,3 +1,27 @@
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+require("telescope").setup({
+  defaults = {
+    sorting_strategy = "ascending",
+    layout_config = {
+      prompt_position = "top",
+    }
+  },
+  pickers = {
+    git_files = {
+      show_untracked = true
+    },
+    find_files = {
+      hidden = true
+    }
+  }
+})
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>lr", builtin.lsp_references, {})
+vim.keymap.set("n", "<leader>ls", builtin.lsp_document_symbols, {})
+vim.keymap.set("n", "<leader>d",
+function()
+  builtin.diagnostics({ bufnr = 0 })
+end, {})
+
