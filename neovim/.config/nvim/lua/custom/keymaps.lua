19f5389c8491df3d6dce133096aaf95d711a7ac5
Connor Meredith <4907463+connormeredith@users.noreply.github.com>
2023-04-23 14:39:26 +0100

    More neovim stuff

--- neovim/.config/nvim/lua/custom/keymaps.lua
+++ neovim/.config/nvim/lua/custom/keymaps.lua
@@ -1,4 +1,8 @@
 vim.g.mapleader = " "
 
--- nvim-tree.
+-- Toggle nvim-tree.
 vim.keymap.set("n", "<leader>e", vim.cmd.NvimTreeToggle)
+
+-- Move lines up and down.
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
--- neovim/.config/nvim/lua/custom/options.lua
+++ neovim/.config/nvim/lua/custom/options.lua
@@ -1,42 +1,33 @@
--- Line numbers.
+vim.g.editorconfig = false
+
 vim.opt.number = true
 vim.opt.relativenumber = true
 
--- Indentation
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
--- Files.
 vim.opt.swapfile = false
 vim.opt.backup = false
 
--- Colors.
 vim.opt.termguicolors = true
 
--- Search.
 vim.opt.hlsearch = false
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
--- Wrapping.
 vim.opt.wrap = false
 
--- Cursor.
 vim.opt.cursorline = true
-
--- Editor.
 vim.opt.scrolloff = 10
+
 vim.opt.signcolumn = "yes"
 
--- Invisibles.
 vim.opt.list = true
 vim.opt.listchars:append "space:⋅"
 vim.opt.listchars:append "eol:↵"
 
--- Clipboard.
 vim.opt.clipboard:append("unnamedplus")
-
--- EditorConfig
-vim.g.editorconfig = false
