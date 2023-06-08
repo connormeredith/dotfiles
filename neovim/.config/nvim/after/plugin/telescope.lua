13fc46ed56f4ca0eb2e2692d9d53504ec612a099
Connor Meredith <4907463+connormeredith@users.noreply.github.com>
2023-06-08 22:32:04 +0100

    Added a few telescope keymaps

--- neovim/.config/nvim/after/plugin/telescope.lua
+++ neovim/.config/nvim/after/plugin/telescope.lua
@@ -4,6 +4,7 @@ require("telescope").setup({
     layout_config = {
       prompt_position = "top",
     },
+    file_ignore_patterns = { "/node_modules/" },
   },
   pickers = {
     git_files = {
@@ -21,12 +22,15 @@ require("telescope").setup({
 })
 
 local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>.", builtin.resume, {})
+vim.keymap.set("n", "<leader>b", builtin.buffers, {})
 vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
 vim.keymap.set("n", "<leader>gs", builtin.git_status, {})
 vim.keymap.set("n", "<leader>lr", builtin.lsp_references, {})
 vim.keymap.set("n", "<leader>ls", builtin.lsp_document_symbols, {})
+vim.keymap.set("n", "<leader>lws", builtin.lsp_dynamic_workspace_symbols, {})
 vim.keymap.set("n", "<leader>d", function()
   builtin.diagnostics({ bufnr = 0 })
 end, {})
