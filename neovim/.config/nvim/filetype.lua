8596fc1506fd0e6a456d7ba7ea8e0e7d08364629
Connor Meredith <4907463+connormeredith@users.noreply.github.com>
2025-09-09 07:59:42 +0100

    added ruby-build deps

--- neovim/.config/nvim/filetype.lua
+++ neovim/.config/nvim/filetype.lua
@@ -4,5 +4,6 @@ vim.filetype.add({
     [".*handlers/.*%.ya?ml"] = "yaml.ansible",
     [".*roles/.*%.ya?ml"] = "yaml.ansible",
     [".*tasks/.*%.ya?ml"] = "yaml.ansible",
+    ["Brewfile"] = "ruby",
   },
 })
