edf8ebdbcc07a5ef14ed8febd206cd170550f0c2
Connor Meredith <4907463+connormeredith@users.noreply.github.com>
2026-04-02 07:20:28 +0100

    add missing neovim packages

--- neovim/.config/nvim/plugin/misc.lua
+++ neovim/.config/nvim/plugin/misc.lua
@@ -2,7 +2,9 @@ vim.pack.add({
   "https://github.com/folke/tokyonight.nvim",
   "https://github.com/christoomey/vim-tmux-navigator",
   "https://github.com/tpope/vim-sleuth",
+  "https://github.com/tpope/vim-surround",
   "https://github.com/airblade/vim-gitgutter",
+  "https://github.com/github/copilot.vim",
 })
 
 vim.cmd.colorscheme("tokyonight-moon")
